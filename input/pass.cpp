16e660c1481ce3b85cb5eff96ff48942f9f6b047
yeah <yeah@yeahs-MacBook-Pro.local>
2022-01-17 23:27:30 -0500

    made small changes to nInput

--- input/pass.cpp
+++ input/pass.cpp
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string>
 
-
 class pass{
     private:
         int specialRan;
--- nInput/pass.cpp
+++ nInput/pass.cpp
@@ -2,136 +2,38 @@
 #include <stdlib.h>
 #include <string>
 
-
 class pass{
     private:
-        char num;
-        char lower;
-        char cap;
-        char special;
-        char special2;
-        char special3;
-        char special4;
-        char comp;
         int specialRan;
         std::string a;
 
     public:
-        void addNum(int count){
-            std::string rep;
-            for(int i = 0; i< count;i++)
-            {
-                num = rand() % 10+48;
-                rep+= num;
-            }
-            a.append(rep);
-        }
 
         void addNum(){
-            std::string rep;
-            num = rand() % 10+48;
-            rep+= num;
-            a.append(rep);
+            char num = rand() % 10+48;
+            a.push_back(num);
         }
-
-        void addlower(int count){
-            std::string rep;
-            for(int i = 0; i< count;i++)
-            {
-                lower = rand() % 26+97;
-                rep += lower;
-            }
-            a.append(rep);
+        void addLower(){
+            char lower = rand() % 26+97;
+            a.push_back(lower);
         }
-
-        void addlower(){
-            std::string rep;
-            lower = rand() % 26+97;
-            rep += lower;
-            a.append(rep);
-        }
-
-        void addcap(int count)
+        void addUpper()
         {
-            std::string rep;
-            for(int i = 0; i< count;i++)
-            {
-                cap = rand() % 26+65; 
-                rep += cap;
-            }
-            a.append(rep);
+            char cap = rand() % 26+65; 
+            a.push_back(cap);
         }
-
-        void addcap()
+        void addSpecial()
         {
-            std::string rep;
-            cap = rand() % 26+65; 
-            rep += cap;
-            a.append(rep);
-        }
-
-        void addspecial(int count)
-        {
-            std::string rep;
-            for(int i = 0; i< count;i++)
-            {            
-                specialRan = rand()% 4;
-                if(specialRan == 0)
-                {
-                    special = rand() % 15+33;
-                    rep+=special;
-                }
-                else if(specialRan == 1)
-                {
-                    special2 = rand() % 6+91; 
-                    rep+=special2;
-
-                }
-                else if(specialRan == 2)
-                {
-                    special3 = rand() % 7+58; 
-                    rep+=special3;
-                }
-                else if(specialRan == 3)
-                {
-                    special4 = rand() % 4+123; 
-                    rep+=special4;
-                }  
-            }
-            a.append(rep);
-        }
-
-        void addspecial()
-        {
-            std::string rep;           
+            char special;
             specialRan = rand()% 4;
             if(specialRan == 0)
-            {
                 special = rand() % 15+33; 
-                rep+=special;
-            }
             else if(specialRan == 1)
-            {
-                special2 = rand() % 6+91; 
-                rep+=special2;
-            }
+                special = rand() % 6+91; 
             else if(specialRan == 2)
-            {
-                special3 = rand() % 7+58; 
-                rep=special3;
-            }
+                special = rand() % 7+58; 
             else if(specialRan == 3)
-            {
-                special4 = rand() % 4+123; 
-                rep+=special4;
-            }  
-
-            a.append(rep);
-        }
-
-
-        void printcap()
-        {
-            std::cout<<a;
+                special = rand() % 4+123; 
+            a.push_back(special);
         }
 };
